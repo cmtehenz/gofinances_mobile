58bd42c58c3d2bd7a76d6dacc59844b4b6d05d15
Gustavo Costa <cmtehenz@gmail.com>
2021-10-08 08:59:43 -0300

    criado navegação

---
+++ src/components/Form/InputForm/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+`;
+
+export const Error = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular };
+  color: ${({ theme }) => theme.colors.attention };
+
+  margin: 7px 0;
+`;
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
-import { Modal } from 'react-native';
+import { 
+  Modal, 
+  TouchableWithoutFeedback, 
+  Keyboard,
+  Alert, 
+} from 'react-native';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
+import { useForm } from 'react-hook-form'
+
+import { InputForm } from '../../components/Form/InputForm'
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input'
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
 
@@ -17,6 +26,23 @@ import {
   TransactionTypes,
 } from './styles';
 
+interface FormData {
+  name: string;
+  amount: string;
+}
+
+const schema = Yup.object().shape({
+  name: Yup
+    .string()
+    .required('Nome é obrigatório'),
+  amount: Yup
+    .number()
+    .typeError('Informe um valor numérico')
+    .positive('O valor não pode ser um numero negativo')
+    .required('Valor obrigatório')
+})
+
+
 export function Register() {
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
@@ -24,6 +50,14 @@ export function Register() {
   const [category, setCategory] = useState({
     key: 'category',
     name: 'Categoria',
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
   });
 
   function handleTransactionTypeSelect(type: 'up' | 'down'){
@@ -38,19 +72,48 @@ export function Register() {
     setCategoryModalOpen(false);
   }
 
+  function handleRegister(form: FormData) {
+    if(!transactionType)
+      return Alert.alert('Selecione o tipo da Transação')
+    
+    if(category.key === 'category')
+      return Alert.alert('Selecione uma categoria');
+
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+    }
+
+    console.log(data)
+  }
+
   return (
-    <Container>
+    <TouchableWithoutFeedback
+      onPress={Keyboard.dismiss}
+    >
+    <Container> 
       <Header>
         <Title>Cadastro</Title>
 
       </Header>
       <Form>
         <Fields>
-          <Input 
+          <InputForm
+            control={control}
+            name="name" 
             placeholder="Nome"
+            autoCapitalize="sentences"
+            autoCorrect={false}
+            error={errors.name && errors.name.message}
           />
-          <Input 
+          <InputForm 
+            control={control}
+            name="amount"
             placeholder="Preço"
+            keyboardType="numeric"
+            error={errors.amount && errors.amount.message}
           />
           <TransactionTypes>
             <TransactionTypeButton 
@@ -72,7 +135,11 @@ export function Register() {
           />
         </Fields>
         
-        <Button title="Enviar"/>
+        <Button 
+          title="Enviar"
+          onPress={handleSubmit(handleRegister)}
+        
+        />
       </Form>
 
       <Modal visible={categoryModalOpen}>
@@ -83,5 +150,6 @@ export function Register() {
         />
       </Modal>
     </Container>
+    </TouchableWithoutFeedback>
   )
 }
