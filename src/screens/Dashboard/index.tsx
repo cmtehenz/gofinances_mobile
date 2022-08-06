2968b342bc037acc39a06c37ecf9c6bc3a6c6d18
Gustavo Costa <cmtehenz@gmail.com>
2022-08-06 19:31:31 -0300

    commit inicial

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+
+import { 
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
+
+export function Dashboard(){
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: 'https://avatars.githubusercontent.com/u/10881123?v=4'}}
+              />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Gustavo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  )
+}
